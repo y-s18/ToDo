0662b4e27b92ee1a66367a4fa0798ed9d65078e8
yamen sahyouni <yamen.sahyouni@gmail.com>
2024-02-11 07:25:58 +0100

    add button and header components

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from "../button/button.component";
+
+@Component({
+    selector: 'toDo-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.scss',
+    imports: [ButtonComponent]
+})
+export class HeaderComponent {
+  title: string = 'TODO';
+
+}
