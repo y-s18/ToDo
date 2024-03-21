6f6df1d8a4982c9e09b9932e3eb5209c2b630322
yamen sahyouni <yamen.sahyouni@gmail.com>
2024-03-21 14:58:24 +0100

    add fontawesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { TodosComponent } from "./components/todos/todos.component";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
     selector: 'toDo-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, TodosComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, TodosComponent, FontAwesomeModule]
 })
 export class AppComponent {
   title = 'toDo';
--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { ToDo } from '../../ToDo';
 import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'toDo-todo-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FaIconComponent],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.scss'
 })
 export class TodoItemComponent {
   @Input() todo!: ToDo;
+  faTimes = faTimes;
   @Output() onToggleDone: EventEmitter<ToDo> = new EventEmitter
 
   onToggle(todo: ToDo){
